f840061988238754edf52531c14e74461e08956b
Kevin Cox <kevincox@kevincox.ca>
2014-05-01 20:43:43 -0400

    Remove index from sample pages.

--- api/post/sample.py
+++ api/post/sample.py
@@ -40,11 +40,6 @@ def post(id, title, content, type=None):
 		p.type = type
 	return p
 
-post("index", "Home", """
-<p>Welcome to the new CSES site!</p>
-<a href="/people">people</a>
-<a href="/login">login</a>
-""", type="page")
 post("hello-world", "Hello, World!", "<p>This is a post!</p>");
 post("first-post", "I am awesome", "<p>f1rs+ p0$t n00b5</p>");
 
