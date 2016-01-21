828972a8b180576945895f100a3fcd7d14789ff3
Kevin Cox <kevincox@kevincox.ca>
2016-01-21 15:02:04 -0500

    Set data dir.

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 import os
 
-os.environ["CSESAPI_DB"] = os.environ['OPENSHIFT_POSTGRESQL_DB_URL']
+os.environ["CSESAPI_DB"]      = os.environ['OPENSHIFT_POSTGRESQL_DB_URL']
+os.environ["CSESAPI_DATADIR"] = os.environ['OPENSHIFT_DATA_DIR'] + "/cses"
 
 from api import app as application
 application.create()
