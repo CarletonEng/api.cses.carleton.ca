17a5ec0191bab6225bdf4ae0642a6125da15c8d4
Kevin Cox <kevincox@kevincox.ca>
2016-01-20 21:33:48 -0500

    Set the database path through the environment.

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 import os
 
+os.environ["CSESAPI_DB"] = os.environ['OPENSHIFT_POSTGRESQL_DB_URL']
+
 from api import app as application
-application.config.database = os.environ['OPENSHIFT_POSTGRESQL_DB_URL']
 application.create()
